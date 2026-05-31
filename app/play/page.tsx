956118fba3f06caf4fc94ae80b34e15895a6711d
Coatt-code <coatt.dev@gmail.com>
2026-05-31 20:40:09 +0500

    add hue control via slider

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,17 +1,24 @@
 'use client';
 
+import Color from 'color';
 import Link from "next/link";
 import { Slider } from "@/components/ui/slider"
 import { useState } from "react";
 
 export default function Page () {
   const [hue, setHue] = useState([0]);
+  function changeHue(hue: any) {
+    setHue(hue)
+  }
     return (
     <div className="h-dvh items-center justify-center gap-4 bg-background p-10">
-      <Slider orientation="vertical" max={100} defaultValue={[0]} onValueChange={(value) => setHue(value)} className="max-h-2/3"/>
+      <Slider orientation="vertical" max={360} defaultValue={[0]} onValueChange={(value) => changeHue(value)} className="max-h-2/3"/>
       <div className="fixed left-10 top-10 p-4 bg-muted">
         <Link href="/">Go back</Link>
-        <div className="h-10 w-10">{hue}</div>
+        <div className="h-10 w-10"
+        style={{ backgroundColor: `hsl(${Number(hue)}, 100%, 50%)` }}>
+          {hue}
+        </div>
       </div>
 
     </div>
--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -38,8 +38,7 @@ function Slider({
     >
       <SliderPrimitive.Track
         data-slot="slider-track"
-        // style={{ '--selected-color': selectedColor } as React.CSSProperties}
-        className="relative grow overflow-hidden bg-[linear-gradient(to_bottom,hsl(0,100%,50%),hsl(60,100%,50%),hsl(120,100%,50%),hsl(180,100%,50%),hsl(240,100%,50%),hsl(300,100%,50%),hsl(360,100%,50%))] rounded-full bg-(--selected-color) data-horizontal:h-2 data-horizontal:w-full data-vertical:h-1/2 data-vertical:w-14"
+        className="relative grow overflow-hidden bg-[linear-gradient(to_top,hsl(0,100%,50%),hsl(60,100%,50%),hsl(120,100%,50%),hsl(180,100%,50%),hsl(240,100%,50%),hsl(300,100%,50%),hsl(360,100%,50%))] rounded-full  data-horizontal:h-2 data-horizontal:w-full data-vertical:h-1/2 data-vertical:w-14"
       >
         <SliderPrimitive.Range
           data-slot="slider-range"
@@ -50,7 +49,7 @@ function Slider({
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
           key={index}
-          className="block h-4 w-4 shrink-0 rounded-full bg-white shadow-sm ring-1 ring-black/10 transition-[color,box-shadow,background-color] select-none not-dark:bg-clip-padding hover:ring-4 hover:ring-ring/30 focus-visible:ring-4 focus-visible:ring-ring/30 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 data-vertical:h-6 data-vertical:w-6"
+          className="block h-4 w-4 shrink-0 rounded-full bg-white select-none ring-0 outline-10 outline-white data-vertical:h-1 data-vertical:w-1"
         />
       ))}
     </SliderPrimitive.Root>
